4b35666035d3f78a93ac3803367d756e6dbcec2f
Alejandro Baez <Alejan.Baez@gmail.com>
2015-01-17 21:38:45 -0500

    reworked the cli

--- lapis_init.lua
+++ lapis_init.lua
@@ -6,51 +6,35 @@
 
 local _CONF = io.popen("echo $HOME"):read() .. "/.lapis_init.conf"
 
-
 --- writes a line to the file selected.
 -- @function write_line
--- @param loc location of the file.
+-- @param dest location of the file.
 -- @param file to write to.
 -- @param str to write.
 -- @param m optional mode for file to write. Defaults to "a+" if left empty.
-function write_line(loc, file, str, m)
-  io.open(loc .. "/" .. file, m or "a+"):write(str):close()
+function write_line(dest, file, str, m)
+  io.open(dest .. "/" .. file, m or "a+"):write(str):close()
 end
 
---- creates the docker container for lapis.
--- @function build_docker
--- @param loc the location of the docker-lapis local copy.
-function build_docker(loc)
-  os.execute("cd " .. loc .. "; docker build -t abaez/lapis .")
+--- gives the string for the specified vcs.
+-- @param vcs choose which DVCS to use. If 'git' not given, then auto to 'hg'.
+function vcs_str(vcs)
+  return vcs == 'git' and
+    "git init; git add -A; git commit -m 'initial commit'" or
+    "hg init; hg commit -Am 'initial commit'"
 end
 
---- checks lpb for new updates.
--- @function has_update
--- @param src project builder installed location
-function has_update(src)
-  for line in io.popen("cd  " .. src .. "; hg incoming"):lines() do
-    if line:match("no changes found") then
-      return false
-    end
-  end
-
-  return true
+--- creates the docker container for lapis from abaez/docker-lapis.
+-- @param cont the docker-lapis source location.
+local function build_docker(cont)
+  os.execute("cd " .. cont .. "; docker build -t abaez/lapis .")
 end
 
 --- creates the intialized directory for the lapis project.
 -- @function build_env
--- @param loc location of the lapis project path.
--- @param template the mercurial repository to use as a project template.
-function build_env(loc, template)
-  local template = template or conf.lpb
-  os.execute(string.format("hg clone %s %s", template, loc))
-  os.remove(loc .. "/lapis_init.lua")
-  os.remove(loc .. "/README.md")
-  write_line(loc, "config.ld", string.format("project = %q", arg[1]))
-  write_line(loc, "fig.yml", "  volumes:\n    - " .. loc .. ":/server")
-  os.execute("rm -rf " .. loc .. "/.hg")
-  os.execute("cd " .. loc ..
-             "; hg init; hg add; hg commit -m 'initial commit'")
+-- @param dest location for the lapis project.
+-- @param templates the templates to use.
+function build_env(dest, templates)
 end
 
 --- simple delay timer.
@@ -58,40 +42,68 @@ function wait(time)
   local time = time or os.time() + 3; while os.time() < time  do end
 end
 
+--- a temporary table for command run.
+-- @param cwd holds the current working directory of lapis_init.
+local tmp = {
+  vcs       = false, -- see @{vcs_str| vcs}.
+  src       = false, -- see @{conf.user| user}
+  name      = false, -- see @{build_env| name}
+  dest      = false, -- see @{build_env| dest}
+  cont      = false, -- see @{build_docker| cont}
+  cwd       = io.popen("pwd"):read() .. "/"
+}
+
 local help = [=[
   lapis_init v1.0
-  usage: lapis_init <name> [-p <path>] [-d <path>]
+  usage: lapis_init <name> [Options]
 
-  Available actions:
-  <name>      name of the lapis project
-  -p <path>   the location of the installation for the project.
+  <name>      name of the lapis project.
+
+  Options:
   -d <path>   makes the docker container. Needs docker-lapis path.
+  -g          use 'git' to instead of 'hg'. Automates to 'hg' if not given.
+  -p <path>   the location for the project to be made.
+  -s <src>    the location of lapis-project-builder source.
   -h          prints this text
 
 ]=]
 
-if #arg == 0 or arg[1] == "-h" then
+if #arg == 0 or arg[1] == '-h' then
   print(help)
+elseif arg[1] == '-s' then
+  get_user(_CONF, arg[2])
 else
+  assert(arg[1] ~= '-d' and arg[1] ~= '-p', help)
+  tmp.name = arg[1]
+
   if #arg > 1 then
     for i = 2, #arg do
-      if arg[i] == '-d' then
-        print("building docker container")
-        wait()
-        build_docker()
-      elseif arg[i] == '-p' then
-        print("making environment build")
-        wait()
-        assert(not has_update(), "You need to update lpb")
-        build_env(arg[i+1] .. "/" .. arg[1], arg[1])
+      if arg[i] == '-p' then
+        tmp.dest = arg[i+1] .. "/" .. tmp.name
+      elseif arg[i]  == '-s' then
+        tmp.src = arg[i+1]
+      elseif arg[i] == 'd'then
+        tmp.cont = arg[i+1]
+      elseif arg[i] == '-g' then
+        tmp.vcs = 'git'
       end
     end
+
+    print("making environment project: " .. tmp.name)
+    build_env(
+      tmp.dest or tmp.cwd .. tmp.name,
+      tmp.name,
+      tmp.src,
+      tmp.cont,
+      get_user(_CONF, tmp.src),
+      tmp.vcs)
   else
-    assert(not has_update(), "You need to update lpb")
-    assert(arg[1] ~= '-d' and arg[1] ~= '-p', help)
-    print("making environment project: " .. arg[1])
-    wait()
-    build_env(string.format(
-             "%s/%s", io.popen("pwd"):read(), arg[1]), arg[1])
+    print("making environment project: " .. tmp.name)
+    build_env(tmp.cwd .. tmp.name,
+              tmp.name,
+              tmp.src,
+              tmp.cont,
+              get_user(_CONF, tmp.src),
+              tmp.vcs)
   end
 end
